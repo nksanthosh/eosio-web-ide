cee01ba465082372a23dcbf53e4eb882d4b54b6a
Santhosh Kumaraswamy <kumaraswamy.santhosh@gmail.com>
2020-06-20 10:48:50 -0400

    Added the ability to increment likes count

--- contract/talk.cpp
+++ contract/talk.cpp
@@ -6,7 +6,7 @@ struct [[eosio::table("message"), eosio::contract("talk")]] message {
     uint64_t    reply_to = {}; // Non-0 if this is a reply
     eosio::name user     = {};
     std::string content  = {};
-    uint64_t    likes    = {};
+    uint64_t    postlikes    = {};
 
     uint64_t primary_key() const { return id; }
     uint64_t get_reply_to() const { return reply_to; }
@@ -45,4 +45,29 @@ class talk : eosio::contract {
             message.content  = content;
         });
     }
+
+ // Like a message and increment the count of likes
+    [[eosio::action]] void like(uint64_t id, eosio::name user) {
+        message_table table{get_self(), 0};
+
+        // Check user
+        require_auth(user);
+
+        //find the row with the post
+        auto itr = table.find(id);
+
+       // if the iterator is not at the end we found the post, so we update the likes count 
+        if (itr != table.end()) {
+            auto postlikes = itr->postlikes;
+            postlikes++;
+
+            // Update the message with the updated count of likes
+            table.modify(itr, get_self(), [&](auto& message) {
+                message.postlikes = postlikes;
+            });
+        }
+    }
+
+    // Similar to the above likes action one can also implement an unlike action
+
 };
